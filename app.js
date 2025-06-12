113793b130b04d92d215999e9d278af10b912e76
Huseyn33 <huseynoyunkanali1@gmail.com>
2025-06-12 15:07:15 +0400

    finish

---
+++ app.js
@@ -0,0 +1,17 @@
+
+// for (let index = 1; index < 5; index++) {
+// console.log(index);
+
+    
+// }
+// for (let index = 0; index < 3 ;index++) {
+
+//     console.log("salam");
+    
+// }
+// for (let index = 0; index <= 4; index++) {
+// console.log(index);
+
+    
+// }
+
