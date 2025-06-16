0b87ad97243de0f9e3d91d7f1975a68621dbca9a
Huseyn33 <huseynoyunkanali1@gmail.com>
2025-06-16 20:14:29 +0400

    finish

--- app.js
+++ app.js
@@ -14,4 +14,52 @@
 
     
 // }
+// task1
+// for (let index = 1; index < 50; index++) {
+
+//     let file=document.getElementById("f").innerText=index;
+    
+    
+// }
+// let eded = prompt("Bir cumle daxil edin icinde eded olsun")
+// let bizimeded=0;
+// while (Bizimeded>eded) {
+  
+    
+// }
+// console.log(Bizimeded);
+// const student = {
+//     name: "Ali",
+//     age: 14,
+//     grade: "8A"
+//   };
+ 
+//   console.log(student.age);
+//   console.log(student.name);
+  
+
+  // const teacher = {
+  //   name: "Günel",
+  //   subject: "Riyaziyyat",
+  //   mail:"Gunell123@Gmail.com"
+  // };
+  // console.log(teacher.mail);
+  
+
+
+
+
+
+// Task3
+
+function Addtax() {
+  let qiymet=[5,10,35]
+for (let index = 0; index < 20; index) {
+console.log(qiymet % index);
+
+  
+}
+  
+};
+Addtax()
 
